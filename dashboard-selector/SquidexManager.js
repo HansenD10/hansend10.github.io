1c8a62e32e8a1985996bcd8f4c3a5981bb4e3502
Dylan Hansen <dhansen@acumium.com>
2021-01-14 13:35:15 -0600

    Add content type to auth request.

--- dashboard-selector/SquidexManager.js
+++ dashboard-selector/SquidexManager.js
@@ -12,7 +12,7 @@ export default class SquidexManager {
     return new Promise((resolve, reject) => {
       fetch(`${squidexUrl}/api/content/${appName}/dashboard`, {
         headers: {
-          Authorization: accessToken
+          Authorization: accessToken,
         }
       })
       .catch(err => reject(err))
@@ -23,17 +23,33 @@ export default class SquidexManager {
 
   getAuthToken() {
     let { squidexUrl, appName, clientId, clientSecret } = this.options;
+    let formBody = [];
+    var postData = {
+      'grant_type': 'client_credentials',
+      'client_id': appName + ':' + clientId,
+      'client_secret': clientSecret,
+      'scope': 'squidex-api',
+    }
 
-    let formData = new FormData;
-    formData.append('grant_type', 'client_credentials');
-    formData.append('client_id', appName + ':' + clientId);
-    formData.append('client_secret', clientSecret);
-    formData.append('scope', 'squidex-api')
+    for (let prop in postData) {
+      let encodedKey = encodeURIComponent(prop);
+      let encodedValue = encodeURIComponent(postData[prop]);
+      formBody.push(encodedKey + '=' + encodedValue);
+    }
+
+    // let formData = new FormData;
+    // formData.set('grant_type', 'client_credentials');
+    // formData.set('client_id', appName + ':' + clientId);
+    // formData.set('client_secret', clientSecret);
+    // formData.set('scope', 'squidex-api')
 
     return new Promise((resolve, reject) => {
       fetch(`${squidexUrl}/identity-server/connect/token`, {
         method: 'POST',
-        body: formData,
+        body: formBody.join('&'),
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
       })
       .catch(err => reject(err))
       .then(res => res.json())
