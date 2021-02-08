7fe888d227b610d1c02be95b586dd88803fd34c9
Dylan Hansen <dhansen@acumium.com>
2021-02-08 10:46:13 -0600

    Fix initial val.

--- dashboard-selector-v2/UIManager.js
+++ dashboard-selector-v2/UIManager.js
@@ -10,14 +10,12 @@ export default class UIManager {
 
   async initializeSquidexManager(ctx) {
     this.formData = {
-      dashboards: ctx.initialContent?.data?.dashboards ?? [],
-      defaultDashboard: ctx.initialContent?.data?.defaultDashboard ?? '',
+      dashboards: ctx.initialContent?.data?.dashboards?.iv?.dashboards ?? [],
+      defaultDashboard: ctx.initialContent?.data?.dashboards?.iv?.defaultDashboard ?? '',
     };
-    console.log(this.formData);
     this.squidexManager = new SquidexManager(this.options);
     this.squidexManager.getAuthToken().then(async (accessToken) => {
       this.data = await this.squidexManager.getDashboards(accessToken);
-      console.log(this.data);
       this.buildDashboardsOptions();
       document.getElementById('dashboard-selector')
         .addEventListener('change', (e) => this.updateValue(e));
@@ -28,19 +26,45 @@ export default class UIManager {
   updateValue(e) {
     let hasVal = this.formData.dashboards.findIndex(s => s.id == e.target.value) !== -1;
 
-    if (hasVal) this.formData.dashboards = this.formData.dashboards.filter(x => x.id !== e.target.value)
-    else this.formData.dashboards.push({ id: e.target.value, name: e.target.id });
-
+    if (hasVal) {
+      this.formData.dashboards = this.formData.dashboards.filter(x => x.id !== e.target.value);
+      document.querySelector(`input[value="${e.target.value}"] ~ button`).style.visibility = 'hidden';
+    }
+    else {
+      this.formData.dashboards.push({ id: e.target.value, name: e.target.id });
+      document.querySelector(`input[value="${e.target.value}"] ~ button`).style.visibility = 'visible';
+    }
+    
     this.form.valueChanged(this.formData);
+  }
+
+  handleDefaultClick(dashboardId) {
+    let defaultDash = document.querySelector(`input[value="${dashboardId}"] ~ button`)
+    let isSelected = this.formData.defaultDashboard === dashboardId;
+
+    this.formData.defaultDashboard = isSelected ? null : dashboardId;
+
+    document.querySelectorAll("input ~ button").forEach(node => {
+      node.style.visibility = isSelected ? 'visible' : 'hidden';
+      node.classList.remove('active');
+    });
+    
+    if (!isSelected) {
+      defaultDash.style.visibility = 'visible';
+      defaultDash.innerText = 'Default Dashboard';
+      defaultDash.classList.add('active');  
+    } else {
+      defaultDash.innerText = 'Make Default Dashboard';
+    }
+    console.log(this.formData);
   }
 
   buildDashboardsOptions() {
     let dashboardSelector = document.getElementById('dashboard-selector');
-    console.log("Before Filter FormData:", this.formData);
     this.formData.dashboards = this.data
       .filter(dashboard => this.formData.dashboards.findIndex(fd => fd.id === dashboard.id) !== -1)
       .map(dashboard => ({ id: dashboard.id, name: dashboard.name }));
-    console.log("After Filter FormData:", this.formData);
+
     this.data.forEach(item => {
       let checkbox = document.createElement('input');
       checkbox.type = 'checkbox';
@@ -56,12 +80,12 @@ export default class UIManager {
       label.appendChild(document.createElement('span'));
       label.appendChild(document.createTextNode(item.name));
 
-      if (!this.formData.defaultDashboard && checkbox.checked) {
-        let setDefaultDashBtn = document.createElement('button');
-        setDefaultDashBtn.innerText = 'Make Default Dashboard';
-        setDefaultDashBtn.type = 'button';
-        label.appendChild(setDefaultDashBtn);
-      }
+      let setDefaultDashBtn = document.createElement('button');
+      setDefaultDashBtn.innerText = 'Make Default Dashboard';
+      setDefaultDashBtn.type = 'button';
+      setDefaultDashBtn.addEventListener('click', (e) => this.handleDefaultClick(item.id));
+      setDefaultDashBtn.style.visibility = checkbox.checked ? 'visible' : 'hidden';
+      label.appendChild(setDefaultDashBtn);
 
       dashboardSelector.appendChild(label);
     });
